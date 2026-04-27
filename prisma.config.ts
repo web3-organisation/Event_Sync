2cd7841b39dcb0320a2a17ef954267ec3dd2e633
Fredo Omar <rabmanomar@gmail.com>
2026-04-27 21:44:52 +0300

    run  database

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
-    schema: 'prisma/schema.prisma',
-    migrations: {
-        path: 'prisma/migrations',
-    },
-    datasource: {
-        url: 'postgresql://events_manager:123456@localhost:5432/eventa_synck_bd',
-    },
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
+  },
+  datasource: {
+    url: 'postgresql://events_manager:123456@localhost:5432/events_synck_bd',
+  },
 });
