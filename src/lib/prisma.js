5995b73f133e2a4d73f474df103a9080fe174094
mioty <hei.mioty.2@gmail.com>
2026-06-09 14:09:23 +0300

    Feat: SearchBarColor modification

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,25 +1,19 @@
-// import { PrismaClient } from "@prisma/client";
-//
-// const globalForPrisma = globalThis;
-//
-// const prisma = globalForPrisma.prisma ?? new PrismaClient();
-//
-// if (process.env.NODE_ENV !== "production") {
-//     globalForPrisma.prisma = prisma;
-// }
-//
-// export default prisma;
-
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
 const connectionString = process.env.DATABASE_URL;
 
-const adapter = new PrismaPg({ connectionString });
+const pool = new Pool({
+    connectionString,
+});
+
+const adapter = new PrismaPg(pool);
 
 const globalForPrisma = globalThis;
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+const prisma =
+    globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") {
     globalForPrisma.prisma = prisma;
