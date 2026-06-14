801a5ab07e4dce5fe19c34fad11c497db2c35738
haja171106 <hei.haja.2@gmail.com>
2026-06-14 14:18:40 +0300

    delete event_sync folder

--- Event_Sync/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // Prisma nécessite d'être côté serveur uniquement
-  serverExternalPackages: ["@prisma/client", "bcryptjs"],
-};
-
-export default nextConfig;
